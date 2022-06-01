6c006e76032ebe9bcd6e5b5d8c0de16319596cc3
datadiscoveries <30391113+datadiscoveries@users.noreply.github.com>
2022-06-01 00:14:32 -0700

    Market Basket Analysis for Online Product Recommendations

---
+++ Market Basket R Code V2.R
@@ -0,0 +1,14 @@
+
+# load libraries
+#install.packages("arules")
+#install.packages("arulesViz")
+library("arules")
+library("arulesViz")
+
+# read in data
+dataset = read.transactions("C:/Users/todda/Desktop/Desktop/Career/Projects and Portfolio/Market Basket/Transaction Data.csv", format = "basket", sep = ",", cols = NULL)
+
+# create Apriori model
+rules = apriori(dataset, parameter = list(supp = 0.001, conf = 0.8, minlen = 1, maxlen = 3), control = list(verbose= FALSE))
+# find top 10 rules sorted by confidence (probability of RHS purchase, given LHS cart) 
+inspect(sort(rules, by="confidence", decreasing = TRUE)[1:10])
