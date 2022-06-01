2fb295e87624cc605b23b71b1e416a9e9e8326ea
datadiscoveries <30391113+datadiscoveries@users.noreply.github.com>
2022-06-01 00:16:59 -0700

    comment formatting

--- Market Basket R Code V2.R
+++ Market Basket R Code V2.R
@@ -1,14 +1,15 @@
 
-# load libraries
+# Load libraries
 #install.packages("arules")
 #install.packages("arulesViz")
 library("arules")
 library("arulesViz")
 
-# read in data
+# Read in data
 dataset = read.transactions("C:/Users/todda/Desktop/Desktop/Career/Projects and Portfolio/Market Basket/Transaction Data.csv", format = "basket", sep = ",", cols = NULL)
 
-# create Apriori model
+# Create Apriori model
 rules = apriori(dataset, parameter = list(supp = 0.001, conf = 0.8, minlen = 1, maxlen = 3), control = list(verbose= FALSE))
-# find top 10 rules sorted by confidence (probability of RHS purchase, given LHS cart) 
+
+# Find top 10 rules sorted by confidence (probability of RHS purchase, given LHS cart) 
 inspect(sort(rules, by="confidence", decreasing = TRUE)[1:10])
